df03dcb124f7968400ce66c74e1347e43996256a
GowriSankar-GSG <thegowrisankar@gmail.com>
2022-12-04 03:42:31 +0530

    Quote_text
    Just a quote by frozen wind

---
+++ Quote_text/Quote_text.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+# create label widget
+myLabel = Label(root, text="""Dearly beloved,
+Times are rough
+The feeling unsteady
+Everyday we battle life
+Sometimes we aren't ready
+But love shook our world
+And touched our hearts
+It has moved mountains
+And now we'll never grow apart
+Yours Truly,
+The frozen wind❤""", font=("Times", 45))
+
+myLabel.pack()
+
+root.mainloop()
